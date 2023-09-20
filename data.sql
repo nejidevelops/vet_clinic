cf71859d32c3831bd9a265a5294bb31fd0608b91
Newton Ombese <newtonombese1@gmail.com>
2023-09-20 15:57:32 +0300

    Add more data to our database

--- data.sql
+++ data.sql
@@ -2,3 +2,11 @@ INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
 INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+
+INSERT INTO animals VALUES (5,'Charmander','2020/02/08',0,FALSE,-11);
+INSERT INTO animals VALUES (6,'Plantmon','2021/11/15',2,TRUE,-5.7);
+INSERT INTO animals VALUES (7,'Squirtle','1993/04/02',3,FALSE,-12.13);
+INSERT INTO animals VALUES (8,'Angemon','2005/06/12',1,TRUE,-45);
+INSERT INTO animals VALUES (9,'Boarmon','2005/06/07',7,TRUE,20.4);
+INSERT INTO animals VALUES (10,'Blossom','1998/10/13',3,TRUE,17);
+INSERT INTO animals VALUES (11,'Ditto','2022/05/14',4,TRUE,22);
