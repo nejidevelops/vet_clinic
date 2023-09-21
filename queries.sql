fa78d8facd0240662d82d1d3c78195fdd11cfbc5
Newton Ombese <newtonombese1@gmail.com>
2023-09-21 09:33:19 +0300

    Adding guided instruction to enhance readability

--- queries.sql
+++ queries.sql
@@ -1,34 +1,90 @@
+-- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
+
+-- List the name of all animals born between 2016 and 2019.
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+-- List the date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name='Pikachu';
+
+-- List name and escape attempts of animals that weigh more than 10.5kg
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+-- Find all animals that are neutered.
 SELECT * FROM animals WHERE neutered = TRUE;
+
+-- Find all animals not named Gabumon.
 SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <=17.3;
 
+--Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
 BEGIN;
 UPDATE animals SET species = 'unspecified';
-ROLLBACK;
-
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name = '%mon';
-UPDATE animals SET species = 'digimon' WHERE name = 'Agumon';
-COMMIT;
-
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022/01/01';
-SELECT * FROM animals;
-SAVEPOINT animal;
-UPDATE animals SET weight_kg = weight_kg * -1;
 SELECT * FROM animals;
 ROLLBACK;
-SELECT * FROM animals;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-SELECT * FROM animals;
-COMMIT;
+
+--Inside a transaction:
+  BEGIN;
+  -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+  UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+  -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+  UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+
+  -- Verify that changes were made.
+  SELECT * FROM animals;
+  -- Commit the transaction.
+  COMMIT;
+  -- Verify that changes persist after commit.
+  SELECT * FROM animals;
+
+
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+  BEGIN;
+  DELETE FROM animals;
+  ROLLBACK;
+-- After the rollback verify if all records in the animals table still exists. After that, you can start breathing as usual ;)
+  SELECT * FROM animals;
+
+-- Inside a transaction:
+  BEGIN;
+
+  -- Delete all animals born after Jan 1st, 2022.
+  DELETE FROM animals WHERE date_of_birth > '2022/01/01';
+
+  -- Create a savepoint for the transaction.
+  SAVEPOINT animal;
+
+  -- Update all animals' weight to be their weight multiplied by -1.
+  UPDATE animals SET weight_kg = weight_kg * -1;
+
+  -- Rollback to the savepoint
+  ROLLBACK;
+
+  BEGIN;
+
+  -- Update all animals' weights that are negative to be their weight multiplied by -1.
+  UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+  -- Commit transaction
+  COMMIT;
+
+-- Write queries to answer the following questions:
+  -- How many animals are there?
+  SELECT COUNT(name) FROM animals;
+
+  -- How many animals have never tried to escape?
+  SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+  -- What is the average weight of animals?
+  SELECT AVG(weight_kg) FROM animals;
+
+  -- Who escapes the most, neutered or not neutered animals?
+  -- What is the minimum and maximum weight of each type of animal?
+  -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 
