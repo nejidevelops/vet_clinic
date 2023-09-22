25497b29ad48c39b14505034579b0f315ab5b3ba
Newton Ombese <newtonombese1@gmail.com>
2023-09-22 22:02:28 +0300

    Work on join tables for visits

--- data.sql
+++ data.sql
@@ -58,3 +58,77 @@ INSERT INTO owners
       WHEN name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom' THEN 4 
       WHEN name = 'Angemon' OR name = 'Boarmon' THEN 5 
     END;
+
+-- Insert the following data for vets:
+  -- Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+  -- Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+  -- Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+  -- Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+    INSERT INTO vets(
+        name,
+        age,
+        date_of_graduation
+      )
+        VALUES
+          ('Vet William Tatcher', 45, '2000/04/23'),
+          ('Vet Maisy Smith', 26, '2019/01/17'),
+          ('Vet Stephanie Mendez', 64, '1981/05/04'),
+          ('Vet Jack Harkness', 38, '2008/06/08')
+    ;
+
+-- Insert the following data for specializations:
+  -- Vet William Tatcher is specialized in Pokemon.
+  -- Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+  -- Vet Jack Harkness is specialized in Digimon.
+  INSERT INTO specializations
+    VALUES
+      (1,1),
+      (1,3),
+      (2,3), 
+      (2,4)
+  ;
+
+-- Insert the following data for visits:
+  -- Agumon visited William Tatcher on May 24th, 2020.
+  -- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+  -- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+  -- Pikachu visited Maisy Smith on Jan 5th, 2020.
+  -- Pikachu visited Maisy Smith on Mar 8th, 2020.
+  -- Pikachu visited Maisy Smith on May 14th, 2020.
+  -- Devimon visited Stephanie Mendez on May 4th, 2021.
+  -- Charmander visited Jack Harkness on Feb 24th, 2021.
+  -- Plantmon visited Maisy Smith on Dec 21st, 2019.
+  -- Plantmon visited William Tatcher on Aug 10th, 2020.
+  -- Plantmon visited Maisy Smith on Apr 7th, 2021.
+  -- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+  -- Angemon visited Jack Harkness on Oct 3rd, 2020.
+  -- Angemon visited Jack Harkness on Nov 4th, 2020.
+  -- Boarmon visited Maisy Smith on Jan 24th, 2019.
+  -- Boarmon visited Maisy Smith on May 15th, 2019.
+  -- Boarmon visited Maisy Smith on Feb 27th, 2020.
+  -- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+  -- Blossom visited Stephanie Mendez on May 24th, 2020.
+  -- Blossom visited William Tatcher on Jan 11th, 2021.
+  INSERT INTO visits
+    VALUES
+      (6, 1, '2020-05-24'),
+      (6, 3, '2020-07-22'),
+      (3, 4, '2021-02-02'),
+      (9, 2, '2020-01-05'),
+      (9, 2, '2020-03-08'),
+      (9, 2, '2020-05-14'),
+      (4, 3, '2021-05-04'),
+      (7, 4, '2021-02-24'),
+      (1, 2, '2019-12-21'),
+      (1, 1, '2020-08-10'),
+      (1, 2, '2021-04-07'),
+      (8, 3, '2019-09-29'),
+      (2, 4, '2020-10-03'),
+      (2, 4, '2020-11-04'),
+      (5, 2, '2019-01-24'),
+      (5, 2, '2019-05-15'),
+      (5, 2, '2020-02-27'),
+      (5, 2, '2020-08-03'),
+      (10, 3, '2020-05-24'),
+      (10, 1, '2021-01-11')
+  ;
--- schema.sql
+++ schema.sql
@@ -39,3 +39,32 @@ CREATE TABLE animals(
 
   -- Add column `owner_id` which is a foreign key referencing the `owners` table
   ALTER TABLE animals ADD COLUMN owner_id INT, ADD CONSTRAINT ownerfk FOREIGN KEY(owner_id) REFERENCES owners(id);  
+
+-- Create a table named `vets` with the following columns:
+    -- `id`: integer (set it as autoincremented PRIMARY KEY)
+    -- `name`: string
+    -- `age`: integer
+    -- `date_of_graduation`: date
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation date
+);
+
+-- There is a **many-to-many** relationship between the tables `species` and `vets`: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called `specializations` to handle this relationship.
+CREATE TABLE specializations(
+    species_id INT,
+    vet_id INT,
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
+
+-- There is a **many-to-many** relationship between the tables `animals` and `vets`: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called `visits` to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits(
+    animal_id INT,
+    vet_id INT,
+    date_of_visit date,
+    CONSTRAINT fk_animal FOREIGN KEY(animal_id) REFERENCES animals(id),
+    CONSTRAINT fk_vet FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
