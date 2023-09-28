bea04a97ec5aa6f913cf5a44c929c6cf8bad5899
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-09-28 09:31:51 +0100

    Alter table by adding data

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -31,3 +31,24 @@ CREATE TABLE "species"(
     "id" BIGINT NOT NULL,
     "name" VARCHAR(255) NOT NULL
 );
+ALTER TABLE
+    "species" ADD PRIMARY KEY("id");
+CREATE TABLE "owners"(
+    "id" INTEGER NOT NULL,
+    "full_name" VARCHAR(255) NOT NULL,
+    "age" INTEGER NOT NULL
+);
+ALTER TABLE
+    "owners" ADD PRIMARY KEY("id");
+ALTER TABLE
+    "specializations" ADD CONSTRAINT "specializations_species_id_foreign" FOREIGN KEY("species_Id") REFERENCES "species"("id");
+ALTER TABLE
+    "visits" ADD CONSTRAINT "visits_vet_id_foreign" FOREIGN KEY("vet_id") REFERENCES "vets"("id");
+ALTER TABLE
+    "specializations" ADD CONSTRAINT "specializations_vet_id_foreign" FOREIGN KEY("vet_id") REFERENCES "vets"("id");
+ALTER TABLE
+    "animals" ADD CONSTRAINT "animals_species_id_foreign" FOREIGN KEY("species_id") REFERENCES "species"("id");
+ALTER TABLE
+    "visits" ADD CONSTRAINT "visits_animal_id_foreign" FOREIGN KEY("animal_id") REFERENCES "animals"("id");
+ALTER TABLE
+    "animals" ADD CONSTRAINT "animals_owner_id_foreign" FOREIGN KEY("owner_id") REFERENCES "owners"("id");
